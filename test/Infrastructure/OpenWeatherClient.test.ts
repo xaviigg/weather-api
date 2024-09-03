ec8bf96a7907b0ea0a48947655ae9e7c78ce2938
Xavi Gallardo <xagallardogallardo1@gmail.com>
2024-09-03 18:26:25 +0200

    add OpenWeatherClient tests

--- test/Infrastructure/OpenWeatherClient.test.ts
+++ test/Infrastructure/OpenWeatherClient.test.ts
@@ -1,37 +1,41 @@
-import axios from 'axios';
-// import OpenWeatherClient from '../../src/Infrastructure/API/OpenWeatherClient';
-// import { OpenWeatherAPIError } from '../../src/Infrastructure/API/Exceptions/OpenWeatherApiError';
-// import Coordinates from '../../src/Domain/ValueObject/Coordinates';
+import axios, { AxiosInstance } from 'axios';
+import OpenWeatherClient from '../../src/Infrastructure/API/OpenWeatherClient';
+import { OpenWeatherAPIError } from '../../src/Infrastructure/API/Exceptions/OpenWeatherApiError';
+import Coordinates from '../../src/Domain/ValueObject/Coordinates';
 
-// jest.mock('axios');
-// const mockedAxios = axios as jest.Mocked<typeof axios>;
-
-// describe('OpenWeatherClient', () => {
-//     const apiKey = 'dummy_api_key';
-//     const client = new OpenWeatherClient(apiKey);
-
-//     it('should fetch weather data successfully', async () => {
-//         const mockResponse = { data: { temp: 20 } };
-//         mockedAxios.get.mockResolvedValue(mockResponse);
-
-//         const result = await client.getWeatherByCoordinates(new Coordinates(41.4, 2.17));
-
-//         expect(result).toEqual(mockResponse.data);
-//     });
-
-//     it('should handle API errors', async () => {
-//         mockedAxios.get.mockRejectedValue({
-//             response: { status: 404, data: 'Not Found' }
-//         });
-
-//         await expect(client.getWeatherByCoordinates(new Coordinates(41.4, 2.17)))
-//             .rejects
-//             .toThrow(OpenWeatherAPIError);
-//     });
-// });
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('OpenWeatherClient', () => {
-    it('should pass', () => {
-        expect(true).toBe(true);
+    const apiKey = 'dummy_api_key';
+    let client = new OpenWeatherClient(apiKey);
+    let axiosInstance: any;
+    beforeEach(() => {
+        mockedAxios.create.mockReturnValue({
+            get: jest.fn(),
+        } as any);
+        client = new OpenWeatherClient('dummy_api_key');
+        axiosInstance = client['axiosInstance'] as jest.Mocked<AxiosInstance>;
+    });
+
+    it('should fetch weather data successfully', async () => {
+        const mockResponse = { data: { temp: 20 }, status: 200 };
+        const coordinates = new Coordinates(41.4, 2.17);
+
+        axiosInstance.get.mockResolvedValue(mockResponse);
+
+        const result = await client.getWeatherByCoordinates(coordinates);
+
+        expect(result).toEqual(mockResponse.data);
+    });
+
+    it('should handle API errors', async () => {
+        axiosInstance.get.mockRejectedValue({
+            response: { status: 404, data: 'Not Found' }
+        });
+
+        await expect(client.getWeatherByCoordinates(new Coordinates(41.4, 2.17)))
+            .rejects
+            .toThrow(OpenWeatherAPIError);
     });
 });
